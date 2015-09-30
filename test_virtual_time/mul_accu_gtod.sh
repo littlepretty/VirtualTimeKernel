0428e867110c37323bd97adc5574132a7703dec1
Jiaqi Yan <littlepretty881203@gmail.com>
2015-09-30 18:52:47 -0500

    Test in make
    Signed-off-by: Jiaqi Yan <littlepretty881203@gmail.com>

--- test_virtual_time/mul_accu_gtod.sh
+++ test_virtual_time/mul_accu_gtod.sh
@@ -2,9 +2,8 @@
 
 make accu_gtod
 OUTPUT=accuracy.log
-sudo ./accu_gtod -t 2 -d -e -p > accuracy.log
-sudo ./accu_gtod -t 4 -d -e -p >> accuracy.log
-sudo ./accu_gtod -t 8 -d -e -p >> accuracy.log
-sudo ./accu_gtod -t 16 -d -e -p >> accuracy.log
-
+sudo ./test_accuracy -t 2 -d -e -p > accuracy.log
+sudo ./test_accuracy -t 4 -d -e -p >> accuracy.log
+sudo ./test_accuracy -t 8 -d -e -p >> accuracy.log
+sudo ./test_accuracy -t 16 -d -e -p >> accuracy.log
 cat accuracy.log
