30772d1a60ee91eeeff85110c7a598bbc4f0f69f
Jiaqi Yan <littlepretty881203@gmail.com>
2015-09-30 12:14:54 -0500

    Start testing dilation
    1. a lot of helpers for dilation and freeze
    2. accuracy test script
    
    Signed-off-by: Jiaqi Yan <littlepretty881203@gmail.com>

---
+++ test_virtual_time/util.h
@@ -0,0 +1,35 @@
+#include <linux/types.h>
+#include <linux/sched.h>
+
+#define NR_ROUND 10000000L
+#define MOUNT_SYS_MIN_VERSION "2.6.35"
+#define USEC_PER_SEC 1000000
+#define CNT_SLEEP 1000000
+
+#define CLONE_NEWTIME 0x40000000
+
+#define NSCLONEFLGS		\
+(				\
+    SIGCHLD         |		\
+    CLONE_NEWNS     |		\
+    CLONE_NEWUTS    |		\
+    CLONE_NEWIPC    |		\
+    CLONE_NEWPID    |		\
+    CLONE_NEWNET		\
+)
+
+// general helpers
+long check_syscall_status(long ret, char* sys_cl_nm);
+long int timeval_to_usec(struct timeval tv);
+int timeval_substract(struct timeval* result, struct timeval* x, struct timeval* y);
+
+// dilation helpers
+int virtual_time_unshare(int flags); // for historical reason
+int set_new_dilation(pid_t pid, int tdf);
+int virtual_time_exit(pid_t pid);
+int show_proc_dilation(pid_t pid);
+
+// freeze helpers
+int freeze_proc(pid_t pid);
+int unfreeze_proc(pid_t pid);
+int show_proc_freeze(pid_t pid);
