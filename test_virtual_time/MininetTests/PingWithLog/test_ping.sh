17180a6ac35199fd24fc448306b9c2f3f7a51f65
Jiaqi Yan <littlepretty881203@gmail.com>
2015-12-16 10:38:54 -0600

    change x-axis name and legend name
    Signed-off-by: Jiaqi Yan <littlepretty881203@gmail.com>

--- test_virtual_time/MininetTests/PingWithLog/test_ping.sh
+++ test_virtual_time/MininetTests/PingWithLog/test_ping.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-rm *.log
-
 SWs="2"
 PingCount=200
 Latencies="5.0 1.0 0.1 0.01 0.001" # microseconds
@@ -18,14 +16,17 @@ do
                         for int in $Intervals 
                         do
                                 echo "***********************************************************"
-                                echo "****** Freeze ${dur}s for every ${int}s with ${delay}ms link ******"
+                                echo "******* Freeze ${dur}s for every ${int}s with ${delay}ms link ******"
                                 echo "***********************************************************"
-                                ./mn_ping.py -n $n --ping_count $PingCount \
-                                        --latency $delay --duration $dur --interval $int
-                                cat Sw${n}Lat${delay}Frz${dur}Int${int}Bsl.log 
-                                cat Sw${n}Lat${delay}Frz${dur}Int${int}Vir.log
+                                #./mn_ping.py -n $n --ping_count $PingCount --latency $delay --duration $dur --interval $int
+                                
+                                ./parse_ping.pl Sw${n}Lat${delay}Frz${dur}Int${int}Bsl.log > Sw${n}Lat${delay}Frz${dur}Int${int}Bsl.ping
+                                ./parse_ping.pl Sw${n}Lat${delay}Frz${dur}Int${int}Vir.log > Sw${n}Lat${delay}Frz${dur}Int${int}Vir.ping
+                                
+                                ./plot_cdf.py --bsl_file Sw${n}Lat${delay}Frz${dur}Int${int}Bsl.ping --vir_file Sw${n}Lat${delay}Frz${dur}Int${int}Vir.ping --label1 "No Freeze: Link Delay=${delay}us" --label2 "Freeze: Duration=${dur}s  Interval=${int}s" --topic_name Sw${n}Lat${delay}Frz${dur}Int${int}
                         done
                 done
         done
 done
+
 
