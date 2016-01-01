28d2ee350d34c059ec572d1306f1b15fe7838e2a
Jiaqi Yan <littlepretty881203@gmail.com>
2016-01-01 17:01:07 -0600

    Repeat 64 runs
    Signed-off-by: Jiaqi Yan <littlepretty881203@gmail.com>

--- test_virtual_time/MininetTests/iPerfWithLog/test_iperf.sh
+++ test_virtual_time/MininetTests/iPerfWithLog/test_iperf.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-read -d '' AwkScript << 'EOF'
-BEGIN {
-        lines=0;
-}
-{
-        ++lines;
-        if (lines >= 14 && lines <= 33) {
-                print $7;
-        }
-}
-END {
-}
-EOF
-
-
 SWs="1"
 PerfTime=30
 bw="100" # mb
+REPEAT=64
 
 Durations="1.0" # seconds
 Intervals="1.0" # 0.5 0.1 0.05" # seconds
@@ -28,28 +14,26 @@ int=1.0
 ovsdb=`pgrep ovsdb-server`
 vswitchd=`pgrep ovs-vswitchd`
 echo "echo 1000 > /proc/${ovsdb}/dilation"
-echo "echo 1000 > /proc/$((${ovsdb}+1))/dilation"
+echo "echo 1000 > /proc/${vswitchd}/dilation"
 echo 1000 > /proc/${ovsdb}/dilation
-#echo 1000 > /proc/$((${ovsdb}+1))/dilation
 echo 1000 > /proc/${vswitchd}/dilation
-#echo 1000 > /proc/$((${vswitchd}+1))/dilation
-
 
 echo "************************************************************"
 echo "****** Freeze ${dur}s for every ${int}s with ${bw}mb link ******"
 echo "************************************************************"
-for ((i = 0; i < 50; ++i))
+for ((i = 0; i < $REPEAT; ++i))
 do
         > Sw${SWs}Bw${bw}Frz${dur}Int${int}Bsl.log
         > Sw${SWs}Bw${bw}Frz${dur}Int${int}Vir.log
         ./mn_iperf.py -n $SWs --perf_time $PerfTime --bw $bw \
                 --duration $dur --interval $int
-        #cat Sw${SWs}Bw${bw}Frz${dur}Int${int}Bsl.log
-        #cat Sw${SWs}Bw${bw}Frz${dur}Int${int}Vir.log
-        awk "$AwkScript" Sw${SWs}Bw${bw}Frz${dur}Int${int}Bsl.log > bsl_run_${i}.txt
-        awk "$AwkScript" Sw${SWs}Bw${bw}Frz${dur}Int${int}Vir.log > vir_run_${i}.txt
+        ./parse_iperf.awk Sw${SWs}Bw${bw}Frz${dur}Int${int}Bsl.log > bsl_run_${i}.txt
+        ./parse_iperf.awk Sw${SWs}Bw${bw}Frz${dur}Int${int}Vir.log > vir_run_${i}.txt
 done
-python avg_plot.py iPerfAvgRate.eps
+
+python avg_plot.py $REPEAT iPerfAvgRate.eps
+
+
 
 #for dur in $Durations
 #do
